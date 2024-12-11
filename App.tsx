177dfa07d72aad398566413e8ad9f96efdab6704
Ngoc H. Le <ngoc.le@NgocLe-MacBook-Pro.local>
2024-12-11 17:32:58 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useCallback} from 'react';
+import {SafeAreaView, ScrollView, View} from 'react-native';
+import {StyleSheet} from 'react-native-unistyles';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const renderRow = useCallback((_: any, index: number) => {
+    return (
+      <View key={index} style={styles.row}>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+      </View>
+    );
+  }, []);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // render
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.fill}>
+      <ScrollView>
+        <View style={styles.container}>
+          {Array.from({length: 100}).fill(0).map(renderRow)}
         </View>
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const styles = StyleSheet.create(({colors}, {screen: {width}}) => ({
+  container: {
+    paddingHorizontal: 16,
+    rowGap: 5,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  fill: {
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  row: {
+    flexDirection: 'row',
+    columnGap: 5,
   },
-  highlight: {
-    fontWeight: '700',
+  box: {
+    width: (width - 32 - 7 * 5) / 8,
+    height: (width - 32 - 7 * 5) / 8,
+    backgroundColor: colors.accent,
   },
-});
+}));
+
+// const styles = StyleSheet.create((theme, {screen: {width}}) => ({
+//   container: {
+//     paddingHorizontal: 16,
+//     rowGap: 5,
+//   },
+//   fill: {
+//     flex: 1,
+//   },
+//   row: {
+//     flexDirection: 'row',
+//     columnGap: 5,
+//   },
+//   box: {
+//     width: (width - 32 - 7 * 5) / 8,
+//     height: (width - 32 - 7 * 5) / 8,
+//     backgroundColor: theme.colors.accent,
+//   },
+// }));
 
 export default App;
